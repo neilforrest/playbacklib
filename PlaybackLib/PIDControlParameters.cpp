c88b0bfd2f64e5623bd9c1a824509e70e7a92e8e
neil.david.forrest <neil.david.forrest@70353906-4c42-0410-b8e8-b7536da5eee0>
2009-03-26 18:02:38 +0000

    Changed Omni PID parameters to be more forcefull

--- PlaybackLib/PIDControlParameters.cpp
+++ PlaybackLib/PIDControlParameters.cpp
@@ -19,16 +19,26 @@ CPIDControlParameters::~CPIDControlParameters(void)
 // PHANToM Omni
 void CPIDControlParameters::SetDevicePhantomOmni ( )
 {//
-	// PID control parameters
 	m_p = 0.3;
-	m_i = 0.0005;
-	m_d = 20.0;
+	m_i = 0.0;
+	m_d = 15.0;
 	m_f = 0.09;
-	m_out_filter = 0.09;
-	m_gain = 0.2;
-	m_sat_low = -3.0;
-	m_sat_high = 3.0;
+	m_out_filter = 0.2;
+	m_gain = 0.4;
+	m_sat_low = -5.0;
+	m_sat_high = 5.0;
 	m_dead_zone = 0.0;
+
+	//// PID control parameters
+	//m_p = 0.3;
+	//m_i = 0.0005;
+	//m_d = 20.0;
+	//m_f = 0.09;
+	//m_out_filter = 0.09;
+	//m_gain = 0.2;
+	//m_sat_low = -3.0;
+	//m_sat_high = 3.0;
+	//m_dead_zone = 0.0;
 }
 
 // PHANToM Premium
